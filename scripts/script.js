0e727201ca4d32450cfda8cc3b2caf28ea79faf2
scmackie <stephen.mackie@quantics.co.uk>
2018-11-15 13:12:48 +0000

    Added basic knockout stuff, still playing around

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,54 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+// Class to represent a row in the seat reservations grid
+function SeatReservation(name, initialMeal) {
+    var self = this;
+    self.name = name;
+    self.meal = ko.observable(initialMeal);
+
+    self.formattedPrice = ko.computed(function() {
+        var price = self.meal().price;
+        return price ? "$" + price.toFixed(2) : "None";        
+    });    
+}
+
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel() {
+    var self = this;
+
+    // Non-editable catalog data - would come from the server
+    self.availableMeals = [
+        { mealName: "Standard (sandwich)", price: 0 },
+        { mealName: "Premium (lobster)", price: 34.95 },
+        { mealName: "Ultimate (whole zebra)", price: 290 }
+    ];    
+
+    self.warbands = [
+    	{ warbandName : "Steelheart's Champions", expansion : "Shadespire", n : 3},
+    	{ warbandName : "Garrick's Reavers", expansion : "Shadespire", n : 5},
+    	{ warbandName : "Sepulchral Guard", expansion : "Shadespire", n : 7}
+    ];
+
+    // Editable data
+    self.seats = ko.observableArray([
+        new SeatReservation("Steve", self.availableMeals[0]),
+        new SeatReservation("Bert", self.availableMeals[0])
+    ]);
+
+    // Computed data
+    self.totalSurcharge = ko.computed(function() {
+       var total = 0;
+       for (var i = 0; i < self.seats().length; i++)
+           total += self.seats()[i].meal().price;
+       return total;
+    });    
+
+    // Operations
+    self.addSeat = function() {
+        self.seats.push(new SeatReservation("", self.availableMeals[0]));
+    }
+    self.removeSeat = function(seat) { self.seats.remove(seat) }
+}
+
+ko.applyBindings(new ReservationsViewModel());
