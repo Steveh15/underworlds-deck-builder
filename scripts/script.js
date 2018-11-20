a98faa9c3093873ade5e0f61a49d57e0cc1939f9
smackie <stephen.c.mackie@gmail.com>
2018-11-20 20:24:55 +0000

    Improved Layout

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,34 @@
 function CardViewerViewModel() {
   var self = this;
 
-  var showWarbandBrowser = ko.observable(true);
+  this.showWarbandBrowser = ko.observable(true);
+  this.showCardView = ko.observable(true);
+  this.showDeckView = ko.observable(false);
+
+
+  this.ViewWarband = function () {
+
+    if (this.showWarbandBrowser() == true) {
+      this.showWarbandBrowser(false);
+    } else {
+      this.showWarbandBrowser(true);
+    }
+  };
+
+  this.cardViewButton = function () {
+    if (this.showCardView() == false) {
+      this.showCardView(true);
+      this.showDeckView(false);
+    }
+  };
+
+  this.deckViewButton = function () {
+    if (this.showDeckView() == false) {
+      this.showCardView(false);
+      this.showDeckView(true);
+    }
+  };
+
 
 }
 
